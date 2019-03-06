9ffc029c621961033839bb7cc12203815e774bac
plotnikovart <artpl98@mail.ru>
2019-03-07 01:08:57 +0300

    Переезд на gradle

---
+++ src/test/java/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    void test() {
+        Assertions.assertFalse(false);
+    }
+
+}
