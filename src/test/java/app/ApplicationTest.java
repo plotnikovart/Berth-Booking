694d6e561a71ebc37a911aa0c767ade94494ea88
plotnikovart <artpl98@mail.ru>
2019-03-19 18:54:05 +0300

    Правки к конфигурации тестов

--- src/test/java/app/ApplicationTest.java
+++ src/test/java/app/ApplicationTest.java
@@ -1,13 +1,12 @@
 package app;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-class ApplicationTest {
-
-    @Test
-    void test() {
-        Assertions.assertFalse(false);
-    }
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@TestPropertySource(locations="classpath:test.properties")
+public abstract class ApplicationTest {
 }
--- src/test/java/app/database/dao/UserDaoTest.java
+++ src/test/java/app/database/dao/UserDaoTest.java
@@ -1,18 +1,13 @@
 package app.database.dao;
 
-import app.Application;
+import app.ApplicationTest;
 import app.database.model.User;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-class UserDaoTest {
+class UserDaoTest extends ApplicationTest {
 
     @Autowired
     private UserDao userDao;
