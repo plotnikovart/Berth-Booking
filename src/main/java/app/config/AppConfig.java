6d0d8b4b33576b13c2e6f28227afd5da1146d5c5
Artem Plotnikov <artpl98@mail.ru>
2019-04-08 00:14:30 +0300

    RequestContextFilter

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,11 +1,13 @@
 package app.config;
 
 import app.common.OperationContext;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +20,14 @@ public class AppConfig implements WebMvcConfigurer {
     @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public OperationContext operationContext() {
         return new OperationContext();
+    }
+
+    @Bean
+    public FilterRegistrationBean addRequestContextFilter() {
+        FilterRegistrationBean<RequestContextFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestContextFilter());
+        registrationBean.setOrder(1);
+        return registrationBean;
     }
 
     @Override
