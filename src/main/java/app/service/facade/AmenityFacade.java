dffa6dad0341ca70825313f23c93a83897fbbc29
Artem Plotnikov <artpl98@mail.ru>
2020-05-07 00:25:57 +0300

    amenity enable

--- src/main/java/app/service/facade/AmenityFacade.java
+++ src/main/java/app/service/facade/AmenityFacade.java
@@ -1,23 +1,27 @@
 package app.service.facade;
 
+import app.database.entity.DictAmenity;
 import app.database.repository.DictAmenityRepository;
 import app.service.berth.dto.DictAmenityDto;
 import app.service.converters.impl.DictAmenityConverter;
 import lombok.RequiredArgsConstructor;
+import one.util.streamex.StreamEx;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class ConvenienceFacade {
+public class AmenityFacade {
 
     private final DictAmenityRepository repository;
     private final DictAmenityConverter converter;
 
     public List<DictAmenityDto> getAll() {
-        var conveniences = repository.findAll();
-        return converter.toDtos(conveniences);
-    }
+        var amenities = StreamEx.of(repository.findAll())
+                .sortedBy(DictAmenity::getKey)
+                .toList();
 
+        return converter.toDtos(amenities);
+    }
 }
