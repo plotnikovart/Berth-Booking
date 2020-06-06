d8428415330fd3dda1a1c8daacc1e1f2a45def99
Artem Plotnikov <artpl98@mail.ru>
2020-06-07 00:47:17 +0300

    password recovery tests

--- src/test/java/ru/hse/coursework/berth/web/AuthControllerTest.java
+++ src/test/java/ru/hse/coursework/berth/web/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package ru.hse.coursework.berth.web;
 
+import one.util.streamex.StreamEx;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,13 +10,19 @@ import ru.hse.coursework.berth.ApplicationTest;
 import ru.hse.coursework.berth.config.security.OperationContext;
 import ru.hse.coursework.berth.database.entity.enums.AccountKind;
 import ru.hse.coursework.berth.database.entity.enums.AccountRole;
+import ru.hse.coursework.berth.database.repository.AccountPasswordRecoveryRepository;
 import ru.hse.coursework.berth.database.repository.AccountRepository;
+import ru.hse.coursework.berth.service.account.AccountService;
 import ru.hse.coursework.berth.service.account.TokenService;
 import ru.hse.coursework.berth.service.account.dto.AccountInfo;
 import ru.hse.coursework.berth.service.account.dto.AuthToken;
+import ru.hse.coursework.berth.service.account.dto.EmailCredential;
 import ru.hse.coursework.berth.service.account.dto.UserInfoDto;
 import ru.hse.coursework.berth.service.account.facebook.FacebookCredential;
 import ru.hse.coursework.berth.service.account.facebook.FacebookUserInfo;
+import ru.hse.coursework.berth.service.account.recovery.dto.PasswordChangeReq;
+import ru.hse.coursework.berth.service.account.recovery.dto.PasswordRecoveryCheckReq;
+import ru.hse.coursework.berth.service.account.recovery.dto.PasswordRecoveryReq;
 
 import java.util.List;
 
@@ -26,11 +33,15 @@ class AuthControllerTest extends ApplicationTest {
     @Autowired
     AccountRepository accountRepository;
     @Autowired
+    AccountPasswordRecoveryRepository accountPasswordRecoveryRepository;
+    @Autowired
     AuthController authController;
     @Autowired
     AccountController accountController;
     @Autowired
     TokenService tokenService;
+    @Autowired
+    AccountService accountService;
 
     private final FacebookUserInfo facebookUserInfo = new FacebookUserInfo()
             .setId(123L)
@@ -43,6 +54,10 @@ class AuthControllerTest extends ApplicationTest {
     @BeforeEach
     public void setUp() {
         super.setUp();
+
+        accountRepository.deleteAll();
+        accountPasswordRecoveryRepository.deleteAll();
+
         Mockito.doReturn(facebookUserInfo).when(facebookAuthClientMock).authenticate(any(), any());
     }
 
@@ -78,5 +93,21 @@ class AuthControllerTest extends ApplicationTest {
         Assertions.assertEquals(expectedAccount, actualAccount);
         Assertions.assertEquals(expectedUserInfo, actualUserInfo);
         Assertions.assertNotNull(expectedUserInfo.getPhoto());
+    }
+
+    @Test
+    void testRecovery() {
+        String email = "a@a.ru";
+
+        accountService.createEmailAccount(email, new byte[]{1, 2}, new byte[]{1, 2});
+
+        authController.recoveryPassword(new PasswordRecoveryReq().setEmail(email));
+        String code = StreamEx.of(accountPasswordRecoveryRepository.findAll()).map(it -> it.getPk().getCode()).findFirst().orElseThrow();
+
+        authController.checkRecoveryPassword(new PasswordRecoveryCheckReq().setEmail(email).setRecoveryCode(code));
+        authController.changePassword(new PasswordChangeReq().setEmail(email).setRecoveryCode(code).setNewPassword("a"));
+
+        authController.login(new EmailCredential().setEmail(email).setPassword("a"), "");
+        Assertions.assertTrue(accountPasswordRecoveryRepository.findAll().isEmpty());
     }
 }
