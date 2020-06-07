13e961b446df14c83c4a407292c9d2aadede1948
Artem Plotnikov <artpl98@mail.ru>
2020-06-07 16:54:04 +0300

    append swagger description

--- src/main/java/ru/hse/coursework/berth/web/WebSocketModelController.java
+++ src/main/java/ru/hse/coursework/berth/web/WebSocketModelController.java
@@ -1,13 +1,13 @@
 package ru.hse.coursework.berth.web;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hse.coursework.berth.websocket.event.incoming.ChatMessageIncomingDto;
 import ru.hse.coursework.berth.websocket.event.incoming.ChatOffsetIncomingDto;
-import ru.hse.coursework.berth.websocket.event.outgoing.ChatMessageOutgoingDto;
-import ru.hse.coursework.berth.websocket.event.outgoing.ReviewPublishOutgoingDto;
+import ru.hse.coursework.berth.websocket.event.outgoing.*;
 
 
 @Api(value = "Описание моделей событий для web socket")
@@ -34,7 +34,25 @@ public class WebSocketModelController {
 
     public static class OutgoingModel {
 
+        @ApiModelProperty(position = 1)
         private ChatMessageOutgoingDto CHAT_MESSAGE;
+
+        @ApiModelProperty(position = 2)
         private ReviewPublishOutgoingDto REVIEW_PUBLISH;
+
+        @ApiModelProperty(position = 3)
+        private BookingOpenOutgoingDto BOOKING_OPEN;
+
+        @ApiModelProperty(position = 4)
+        private BookingApproveOutgoingDto BOOKING_APPROVE;
+
+        @ApiModelProperty(position = 5)
+        private BookingRejectOutgoingDto BOOKING_REJECT;
+
+        @ApiModelProperty(position = 6)
+        private BookingPayOutgoingDto BOOKING_PAY;
+
+        @ApiModelProperty(position = 7)
+        private BookingCancelOutgoingDto BOOKING_CANCEL;
     }
 }
